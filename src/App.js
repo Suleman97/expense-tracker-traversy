44a59f8a7c7ee027140f58db67960cf90af4af68
Muhammad Suleman <54745700+Suleman97@users.noreply.github.com>
2021-06-24 19:15:57 +0500

    setup Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeBalance } from './components/IncomeBalance';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Header />
+      {/* <div className='container'>
+        <Balance />
+      </div>
+      <IncomeBalance /> */}
     </div>
   );
 }
